23a9614b3d73ec4ca363d0cf897baf9cd2428cd9
Jinacker <rlawls1448@naver.com>
2025-10-11 13:20:07 +0900

    [FEAT] DB 연결 테스트

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 // const express = require('express')  // -> CommonJS
 import express from 'express'          // -> ES Module
 
+// 환경 변수 로드
+import dotenv from 'dotenv';
+dotenv.config();
+
+// MySQL import
+import mysql from 'mysql2/promise';
+
+
 const app = express()
 const port = 3000
+
+// 환경변수 로드해서 DB 커넥션 풀 연결
+const db = await mysql.createConnection(process.env.DB_URL);
+console.log("MySQL 연결 성공!");
 
 app.get('/', (req, res) => {
   res.send('Hello Worldz')
