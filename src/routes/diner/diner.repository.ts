30b2dc84b3e9fba67f15cb7426003903de702e19
Jinacker <rlawls1448@naver.com>
2025-11-09 16:44:12 +0900

    [FIX] 타입 제약 느슨하게 풀어줌

--- src/routes/diner/diner.repository.ts
+++ src/routes/diner/diner.repository.ts
@@ -16,7 +16,9 @@ export const addDinerToDB = async(regionId:number, data: AddDinerRequest) => {
         rating: data.rating ?? 0
         }
     });
+
     return addedDinerToDB;
+
   } catch(error) {
     throw(error);
   }
@@ -32,34 +34,9 @@ export const getDinerById = async (dinerId: number) => {
     const dinerDetail = await prisma.diner.findUnique({
       where : {id:dinerId}
     });
+    return dinerDetail;
   } catch(error) {
     throw(error);
   }
 }
 
-
-// export const getDinerById = async (dinerId: number): Promise<DinerFromDB | null> => {
-//   const conn = await pool.getConnection();
-//   try {
-//     const [rows] = await pool.query<RowDataPacket[]>(
-//       `
-//       SELECT d.*, f.name AS food_category_name
-//       FROM diner d
-//       JOIN food_category f ON d.category_id = f.id
-//       WHERE d.id = ?;
-//       `,
-//       [dinerId]
-//     );
-
-//     if (rows.length === 0) return null;
-//     return rows[0] as DinerFromDB;
-//   } catch (err) {
-//     throw new Error(
-//       `식당 조회 중 오류가 발생했습니다. (${
-//         err instanceof Error ? err.message : String(err)
-//       })`
-//     );
-//   } finally {
-//     conn.release();
-//   }
-// };
