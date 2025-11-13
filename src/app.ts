20426566a230bc738f44322a34aeb2716e76f12d
Jinacker <rlawls1448@naver.com>
2025-11-13 13:31:03 +0900

    [FEAT] 인증 미들웨어 실습

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 // ===== 미들웨어 설정 =====
 app.use(cors());
 app.use(express.static("public"));
-app.use(express.json());
+app.use(express.json()); // body parser (express 내장)
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev")); // HTTP 요청 로깅용 => 매우매우 유용하네
 app.use(cookieParser()); // 쿠키 parser
@@ -45,6 +45,58 @@ app.get('/getcookie', (req, res) => {
         res.send('쿠키가 없습니다.');
     }
 });
+
+const isLogin = (req, res, next) => {
+    // cookie-parser가 만들어준 req.cookies 객체에서 username을 확인
+    const { username } = req.cookies; 
+
+    if (username) {
+        console.log(`[인증 성공] ${username}님, 환영합니다.`);
+        next(); 
+    } else {
+    
+        console.log('[인증 실패] 로그인이 필요합니다.');
+        res.status(401).send('<script>alert("로그인이 필요합니다!");location.href="/login";</script>');
+    }
+};
+
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>메인 페이지</h1>
+        <p>이 페이지는 로그인이 필요 없습니다.</p>
+        <ul>
+            <li><a href="/mypage">마이페이지 (로그인 필요)</a></li>
+        </ul>
+    `);
+});
+
+
+app.get('/login', (req, res) => {
+    res.send('<h1>로그인 페이지</h1><p>로그인이 필요한 페이지에서 튕겨나오면 여기로 옵니다.</p>');
+});
+
+
+app.get('/mypage', isLogin, (req, res) => {
+    res.send(`
+        <h1>마이페이지</h1>
+        <p>환영합니다, ${req.cookies.username}님!</p>
+        <p>이 페이지는 로그인한 사람만 볼 수 있습니다.</p>
+    `);
+});
+
+
+app.get('/set-login', (req, res) => {
+    res.cookie('username', 'UMC9th', { maxAge: 3600000 });
+    res.send('로그인 쿠키(username=UMC9th) 생성 완료! <a href="/mypage">마이페이지로 이동</a>');
+});
+
+
+app.get('/set-logout', (req, res) => {
+    res.clearCookie('username');
+    res.send('로그아웃 완료 (쿠키 삭제). <a href="/">메인으로</a>');
+});
+
 // ===== 실습 ======
 
 
