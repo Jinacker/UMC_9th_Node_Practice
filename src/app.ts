2902d1a95d211445b849124f717b2716bc609f94
Jinacker <rlawls1448@naver.com>
2025-11-13 13:27:15 +0900

    [FEAT] cookie parser 설치 및 테스트

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import reviewRouter from "./routes/review/review.router.js";
 import dinerRouter from "./routes/diner/diner.router.js";
 import missionRouter from "./routes/mission/mission.router.js";
 import userMissionRouter from "./routes/user-mission/user-mission.router.js";
+import cookieParser from 'cookie-parser';
+
 
 import { errorHandler } from "./middleware/error-handler.js";
 import morgan from "morgan";
@@ -18,8 +20,33 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev")); // HTTP 요청 로깅용 => 매우매우 유용하네
+app.use(cookieParser()); // 쿠키 parser
+
 
 // ===== 라우터 설정 =====
+
+// ===== 실습 ======
+// 쿠키 만드는 라우터 
+app.get('/setcookie', (req, res) => {
+    // 'myCookie'라는 이름으로 'hello' 값을 가진 쿠키를 생성
+    res.cookie('myCookie', 'hello', { maxAge: 60000 }); // 60초간 유효
+    res.send('쿠키가 생성되었습니다!');
+});
+
+// 쿠키 읽는 라우터 
+app.get('/getcookie', (req, res) => {
+    // cookie-parser 덕분에 req.cookies 객체에서 바로 꺼내 쓸 수 있음
+    const myCookie = req.cookies.myCookie; 
+    
+    if (myCookie) {
+        console.log(req.cookies); // { myCookie: 'hello' }
+        res.send(`당신의 쿠키: ${myCookie}`);
+    } else {
+        res.send('쿠키가 없습니다.');
+    }
+});
+// ===== 실습 ======
+
 
 // 헬스 체크
 app.get("/", (_req: Request, res: Response) => {
