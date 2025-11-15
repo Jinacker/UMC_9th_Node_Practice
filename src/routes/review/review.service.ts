c9b19f169cf597aa8e820c8d2cb47f449fd2c29e
Jinacker <rlawls1448@naver.com>
2025-11-16 01:04:47 +0900

    [FEAT] Review 관련 커스텀 에러 셋팅

--- src/routes/review/review.service.ts
+++ src/routes/review/review.service.ts
@@ -1,4 +1,6 @@
 // service => 실제 비즈니스 로직 실행
+import { DinerNotFoundError } from "../../errors/dinerError.js";
+import { ReviewAddFailedError, ReviewLoadFailedError } from "../../errors/reviewError.js";
 import { responseFromReview } from "./review.dto.js";
 import { addReviewToDB, getReviewById, checkDinerExists, getAllDinerReviews, getAllMyReviews } from "./review.repository.js";
 import { AddReviewRequest, ReviewResponseDTO } from "./review.types.js";
@@ -10,7 +12,7 @@ export const addReview = async (
   // 1. 가게 존재 여부 검증
   const dinerExists = await checkDinerExists(dinerId);
   if (!dinerExists) {
-    throw new Error(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
+    throw new DinerNotFoundError(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
   }
 
   // 2. Repository에 DB 저장 요청
@@ -21,14 +23,14 @@ export const addReview = async (
   });
 
   if (!addedReview) {
-    throw new Error("리뷰 등록에 실패했습니다.");
+    throw new ReviewAddFailedError("리뷰 등록에 실패했습니다.");
   }
 
   // 3. 방금 등록한 리뷰 데이터 조회
   const review = await getReviewById(addedReview.id);
 
   if (!review) {
-    throw new Error("등록된 리뷰 정보를 불러올 수 없습니다.");
+    throw new ReviewLoadFailedError("등록된 리뷰 정보를 불러올 수 없습니다.");
   }
 
   // 4. 응답용 DTO 변환 후 반환
@@ -48,7 +50,7 @@ export const listUserReviews = async(userId:number, cursor: number) => {
   const myReviews = await getAllMyReviews(userId,cursor);
 
   if (!myReviews) {
-    throw new Error("내 리뷰 작성 목록 불러오기를 실패했습니다."); // controller로 에러를 throw해줌.
+    throw new ReviewLoadFailedError("내 리뷰 작성 목록 불러오기를 실패했습니다."); // controller로 에러를 throw해줌.
   }
 
   return myReviews.map(responseFromReview); // Repository에서 받아온 리뷰 배열 => DTO에 map으로 각각 파싱
