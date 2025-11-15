43b87dfdc20a057bb1df7f043a12b1967a467c6f
Jinacker <rlawls1448@naver.com>
2025-11-16 01:12:11 +0900

    [FEAT] User-Mission 도메인 관련 커스텀 에러 셋팅

--- src/routes/user-mission/user-mission.service.ts
+++ src/routes/user-mission/user-mission.service.ts
@@ -1,5 +1,6 @@
 // service => 실제 비즈니스 로직 실행
 
+import { AlreadyChallengeError, FailedChallengeError, FailedLoadChallengeLog, NotChallengedMissionError, NotYourMissionError } from "../../errors/userMissionError.js";
 import { responseFromMissionClearLog,responseFromMyMissionList, responseForClearLog } from "./user-mission.dto.js";
 import {
   checkOngoingMission,
@@ -19,21 +20,21 @@ export const challengeMission = async (
   const isOngoing = await checkOngoingMission(data.userId, dinerMissionId);
 
   if (isOngoing) {
-    throw new Error("이미 도전 중인 미션입니다.");
+    throw new AlreadyChallengeError("이미 도전 중인 미션입니다.");
   }
 
   // 2. Repository에 미션 도전 로그 추가
   const logId = await addMissionClearLog(data.userId, dinerMissionId);
 
   if (!logId) {
-    throw new Error("미션 도전 등록에 실패했습니다.");
+    throw new FailedChallengeError("미션 도전 등록에 실패했습니다.");
   }
 
   // 3. 방금 등록한 미션 도전 로그 조회
   const missionLog = await getMissionClearLogById(logId);
 
   if (!missionLog) {
-    throw new Error("등록된 미션 도전 정보를 불러올 수 없습니다.");
+    throw new FailedLoadChallengeLog("등록된 미션 도전 정보를 불러올 수 없습니다.");
   }
 
   // 4. 응답용 DTO 변환 후 반환
@@ -62,13 +63,13 @@ export const completeMissionService = async (userId:number, missionLogId: number
   // 1. 일단 해당 유저의 미션이 맞는지 체크
   const checkUsers = await checkUser(userId,missionLogId);
   if (!checkUsers) {
-    throw new Error("해당 유저의 미션이 아닙니다.")
+    throw new NotYourMissionError("해당 유저의 미션이 아닙니다.")
   }
 
   // 2. 미션이 진행중인지 체크
   const checkMission= await checkMissionProgress(missionLogId);
   if (!checkMission) {
-    throw new Error("도전하지 않은 미션입니다.")
+    throw new NotChallengedMissionError("도전하지 않은 미션입니다.")
   }
 
   // 3. 해당 미션을 완료
