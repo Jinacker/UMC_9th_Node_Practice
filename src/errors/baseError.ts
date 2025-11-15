6cdfd00c7732b21cfc0965d9814e017e606e5f60
Jinacker <rlawls1448@naver.com>
2025-11-16 01:18:31 +0900

    [FEAT] 에러 상속 할때 프로토 타입 추가

--- src/errors/baseError.ts
+++ src/errors/baseError.ts
@@ -9,5 +9,8 @@ export class AppError extends Error {
     this.status = status;
     this.errorCode = errorCode;
     this.data = data;
+
+    // JS에서 Error 상속할 때 반드시 추가해야 함
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
