e7554b82c0228a04882fa05b6fe4a4413cc75f71
Jinacker <rlawls1448@naver.com>
2025-11-16 00:23:23 +0900

    [REFACTOR] 전역 응답 타입 선언

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -2,6 +2,7 @@ import "express-serve-static-core";
 
 // 글로벌 타입 선언
 // 성공 응답 타입 선언
+// .sucess로 쓰기 위해서 이렇게 express 내장으로 모듈을 추가해주는것!
 declare module "express-serve-static-core" {
   interface Response {
     success: (data: any) => Response;
