dca66c5135ae2c197642d5be0151da5fefb178e2
Jinacker <rlawls1448@naver.com>
2025-11-16 01:01:31 +0900

    [FEAT] Mission 도메인 관련 커스텀 에러 셋팅

--- src/routes/mission/mission.service.ts
+++ src/routes/mission/mission.service.ts
@@ -1,4 +1,6 @@
 // service => 실제 비즈니스 로직 실행
+import { DinerNotFoundError } from "../../errors/dinerError.js";
+import { CantFindDinerMissionError, CantRegisterMissionError, MissionNotFoundError } from "../../errors/missionError.js";
 import { responseFromDinerMission, responseFromDinerMissionList } from "./mission.dto.js";
 import {
   addDinerMissionToDB,
@@ -16,13 +18,13 @@ export const addDinerMission = async (
   // 1. 가게 존재 여부 검증
   const dinerExists = await checkDinerExists(dinerId);
   if (!dinerExists) {
-    throw new Error(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
+    throw new DinerNotFoundError(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
   }
 
   // 2. 미션 존재 여부 검증
   const missionExists = await checkMissionExists(data.missionId);
   if (!missionExists) {
-    throw new Error(`ID가 ${data.missionId}인 미션을 찾을 수 없습니다.`);
+    throw new MissionNotFoundError(`ID가 ${data.missionId}인 미션을 찾을 수 없습니다.`);
   }
 
   // 3. Repository에 DB 저장 요청
@@ -42,14 +44,14 @@ export const addDinerMission = async (
   const dinerMissionId = await addDinerMissionToDB(dinerId, requestData);
 
   if (!dinerMissionId) {
-    throw new Error("가게 미션 등록에 실패했습니다.");
+    throw new CantRegisterMissionError("가게 미션 등록에 실패했습니다.");
   }
 
   // 4. 방금 등록한 가게 미션 데이터 조회
   const dinerMission = await getDinerMissionById(dinerMissionId);
 
   if (!dinerMission) {
-    throw new Error("등록된 가게 미션 정보를 불러올 수 없습니다.");
+    throw new CantFindDinerMissionError("등록된 가게 미션 정보를 불러올 수 없습니다.");
   }
 
   // 5. 응답용 DTO 변환 후 반환
@@ -67,7 +69,7 @@ export const listDinerMission = async (dinerId: number, cursor: number): Promise
   // 1. 가게 존재 여부 검증
   const dinerExists = await checkDinerExists(dinerId);
   if (!dinerExists) {
-    throw new Error(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
+    throw new DinerNotFoundError(`ID가 ${dinerId}인 가게를 찾을 수 없습니다.`);
   }
 
   // 2. 해당 가게의 미션 목록 조회
